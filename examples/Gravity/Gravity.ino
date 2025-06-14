07ed4b3d9ac164643cc97184288d4fedbf3cfb36
Adam Wonak <adam.wonak@gmail.com>
2025-06-14 21:52:18 +0000

    Update Font (#5)
    Reduce the amount of program memory used by switching to original firmware's optimized fonts.
    
    Reviewed-on: https://git.pinkduck.xyz/adam/libGravity/pulls/5
    Co-authored-by: Adam Wonak <adam.wonak@gmail.com>
    Co-committed-by: Adam Wonak <adam.wonak@gmail.com>

--- examples/Gravity/Gravity.ino
+++ examples/Gravity/Gravity.ino
@@ -48,8 +48,52 @@ enum ParamsChannelPage {
     PARAM_CH_LAST,
 };
 
-const auto TEXT_FONT = u8g2_font_missingplanet_tr;
-const auto LARGE_FONT = u8g2_font_maniac_tr;
+const PROGMEM uint8_t TEXT_FONT[437] U8G2_FONT_SECTION("velvetscreen") = 
+  "\64\0\2\2\3\3\2\3\4\5\5\0\0\5\0\5\0\0\221\0\0\1\230 \4\200\134%\11\255tT"
+  "R\271RI(\6\252\334T\31)\7\252\134bJ\12+\7\233\345\322J\0,\5\221T\4-\5\213"
+  "f\6.\5\211T\2/\6\244\354c\33\60\10\254\354T\64\223\2\61\7\353\354\222\254\6\62\11\254l"
+  "\66J*\217\0\63\11\254l\66J\32\215\4\64\10\254l\242\34\272\0\65\11\254l\206\336h$\0\66"
+  "\11\254\354T^\61)\0\67\10\254lF\216u\4\70\11\254\354TL*&\5\71\11\254\354TL;"
+  ")\0:\6\231UR\0A\10\254\354T\34S\6B\11\254lV\34)\216\4C\11\254\354T\324\61"
+  ")\0D\10\254lV\64G\2E\10\254l\206\36z\4F\10\254l\206^\71\3G\11\254\354TN"
+  "\63)\0H\10\254l\242\34S\6I\6\251T\206\0J\10\254\354k\231\24\0K\11\254l\242J\62"
+  "\225\1L\7\254lr{\4M\11\255t\362ZI\353\0N\11\255t\362TI\356\0O\10\254\354T"
+  "\64\223\2P\11\254lV\34)g\0Q\10\254\354T\264b\12R\10\254lV\34\251\31S\11\254\354"
+  "FF\32\215\4T\7\253dVl\1U\10\254l\242\63)\0V\11\255t\262Ne\312\21W\12\255"
+  "t\262J*\251.\0X\11\254l\242L*\312\0Y\12\255tr\252\63\312(\2Z\7\253df*"
+  "\7p\10\255\364V\266\323\2q\7\255\364\216\257\5r\10\253d\242\32*\2t\6\255t\376#w\11"
+  "\255\364V\245FN\13x\6\233dR\7\0\0\0\4\377\377\0";
+
+const PROGMEM uint8_t LARGE_FONT[916] U8G2_FONT_SECTION("stk-l") = 
+  "#\0\4\4\4\5\2\1\6\17\30\1\0\27\0\0\0\1\77\0\0\3w%'\17\37\313\330R#&"
+  "\32!F\14\211I\310\24!\65\204(MF\21)Cd\304\10\62b\14\215\60Vb\334\20\0/\14"
+  "\272\336\336d\244\350\263q\343\0\60\37|\377\216!%*\10\35\263\253ChD\30\21bB\14\242S"
+  "\306lv\210\204\22Ef\0\61\24z\337\322\60R\205\314\234\31\61F\310\270\371\177\224\42\3\62\33|"
+  "\377\216)\64*\10\35\63\66r\206\304\314`c\252\34\301\221\263|\360\300\0\63\34|\377\216)\64*"
+  "\10\35\63\66r \71\332YIr\226\306\16\221P\203\312\14\0\64 |\377\226\220AC\306\20\31B"
+  "f\310\240\21\204F\214\32\61j\304(cv\366\200\305\312\371\0\65\32|\377\206\212-F\316\27\204\224"
+  "\254\30\65t\344,\215\35\42\241\6\225\31\0\66\33}\17\317\251\64+\206\235\63:/\314,aA\352"
+  "\234\335\235\42\261&\325\31\0\67\23|\377\302\212\7)\347Crt\70\345\300\221\363\16\0\70 |\377"
+  "\216)\64*\10\35\263\354\20\11\42d\20\235BC\204\4\241cvv\210\204\32Tf\0\71\32|\377"
+  "\216)\64*\10\35\263\263C$\226\250I\71_\14\42\241\6\225\31\0A\26}\17S\271Si(\31"
+  "\65d\324\210q\366\356\301w\366\273\1B$}\17C\42\65KF\221\30\66b\330\210a#\206\215\30"
+  "Eb\311&\243H\14;g\317\36\204`\261\4\0C\27}\17\317\251\64K\10!\63:\377\247\304F"
+  "\20\42\261F\21\22\0D\33}\17C\42\65KF\15\31\66b\330\210q\366\77;\66b\24\211%j"
+  "\22\1E\21|\377\302\7)\347%\42\214F\316/\37<\60F\20|\377\302\7)\347\313\64\331\214\234"
+  "\177\11\0G\31\216\37\17*\65L\206\35\264v>\322\241\15\217\221 \65\204\215\262\63\0H\17|\377"
+  "\302\60\373g\17\36\60\263\177\66\0I\7so\302\37$J\22|\377\346\374\377\322\230\261C\210H\250"
+  "Ae\6\0K\42|\377\302\60S\247F\14\42\61h\310\30\42c&!\63\202\320\251\64JV\14\42"
+  "\61\352\230\375l\0L\15{\357\302\300\371\377\37>x\60\0M$}\17\203\310r\346N\245Q\263\202"
+  "E\12)L\224\60Q\302\310\20#C\214\14\61\23\306L\30s\366\335\0N#}\17\203@s\346\216"
+  "\35C\205*Q\42\23cL\214\61\62\304\310\20\63#\314\214\60\224\25f\327\231\33O\26}\17\317\251"
+  "\64KF\215\30g\377\337\215\30\65dM\252\63\0P\26|\377B\32%+F\35\263W\207H\254H"
+  "\203h\344\374%\0Q\31}\17S\261\64KF\215\30g\377oF\230\31q\246\210\42E%F\0R"
+  "\61\216\37\203\242\65L\206\221\30\67b\334\210q#\306\215\30\67b\30\211QD\230(J\65d\330\230"
+  "Qc\10\315j\314(\42\303H\214\33\61\356\340\0S!\216\37\317\261DKH\221\30\67b\334\210\261"
+  "c)M\246Ji\331\331\32\64\207\212D\223Uh\0T\15}\17\303\7\251\206\316\377\377\12\0U\21"
+  "|\377\302\60\373\377\317F\14\32\242\6\225\31\0X)~\37\303@\203\307H\14\33B\210\14\21RC"
+  "\206\241\63h\222(I\203\346\220\15\31E\204\14!\42\303F\20;h\341\0x\24\312\336\302 CG"
+  "H\240\61E\312\14\222)\6Y\64\0\0\0\0\4\377\377\0";
 
 #define play_icon_width 14
 #define play_icon_height 14
@@ -252,7 +296,7 @@ void ResetOutputs() {
 // Constants for screen layout and fonts
 constexpr int SCREEN_CENTER_X = 32;
 constexpr int MAIN_TEXT_Y = 26;
-constexpr int SUB_TEXT_Y = 42;
+constexpr int SUB_TEXT_Y = 40;
 constexpr int VISIBLE_MENU_ITEMS = 3;
 constexpr int MENU_ITEM_HEIGHT = 14;
 constexpr int MENU_BOX_PADDING = 4;
@@ -298,7 +342,7 @@ void DisplayMainPage() {
             switch (app.selected_source) {
                 case SOURCE_INTERNAL:
                     sprintf(mainText, "%s", "INT");
-                    subText = "Clock";
+                    subText = "CLOCK";
                     break;
                 case SOURCE_EXTERNAL_PPQN_24:
                     sprintf(mainText, "%s", "EXT");
@@ -319,7 +363,7 @@ void DisplayMainPage() {
     drawCenteredText(subText, SUB_TEXT_Y, TEXT_FONT);
 
     // Draw Main Page menu items
-    const char* menu_items[PARAM_MAIN_LAST] = {"Tempo", "Source"};
+    const char* menu_items[PARAM_MAIN_LAST] = {"TEMPO", "SOURCE"};
     drawMenuItems(menu_items, PARAM_MAIN_LAST);
 }
 
@@ -342,30 +386,30 @@ void DisplayChannelPage() {
             int mod_value = ch.getClockMod(withCvMod);
             if (mod_value > 1) {
                 sprintf(mainText, "/%d", mod_value);
-                subText = "Divide";
+                subText = "DIVIDE";
             } else {
                 sprintf(mainText, "x%d", abs(mod_value));
-                subText = "Multiply";
+                subText = "MULTIPLY";
             }
             break;
         }
         case PARAM_CH_PROB:
             sprintf(mainText, "%d%%", ch.getProbability(withCvMod));
-            subText = "Hit Chance";
+            subText = "HIT CHANCE";
             break;
         case PARAM_CH_DUTY:
             sprintf(mainText, "%d%%", ch.getDutyCycle(withCvMod));
-            subText = "Pulse Width";
+            subText = "PULSE WIDTH";
             break;
         case PARAM_CH_OFFSET:
             sprintf(mainText, "%d%%", ch.getOffset(withCvMod));
-            subText = "Shift Hit";
+            subText = "SHIFT HIT";
             break;
         case PARAM_CH_CV_SRC: {
             switch (ch.getCvSource()) {
                 case CV_NONE:
                     sprintf(mainText, "SRC");
-                    subText = "None";
+                    subText = "NONE";
                     break;
                 case CV_1:
                     sprintf(mainText, "SRC");
@@ -382,23 +426,23 @@ void DisplayChannelPage() {
             switch (ch.getCvDestination()) {
                 case CV_DEST_NONE:
                     sprintf(mainText, "DEST");
-                    subText = "None";
+                    subText = "NONE";
                     break;
                 case CV_DEST_MOD:
                     sprintf(mainText, "DEST");
-                    subText = "Clock Mod";
+                    subText = "CLOCK MOD";
                     break;
                 case CV_DEST_PROB:
                     sprintf(mainText, "DEST");
-                    subText = "Probability";
+                    subText = "PROBABILITY";
                     break;
                 case CV_DEST_DUTY:
                     sprintf(mainText, "DEST");
-                    subText = "Duty Cycle";
+                    subText = "DUTY CYCLE";
                     break;
                 case CV_DEST_OFFSET:
                     sprintf(mainText, "DEST");
-                    subText = "Offset";
+                    subText = "OFFSET";
                     break;
             }
             break;
@@ -410,7 +454,7 @@ void DisplayChannelPage() {
 
     // Draw Channel Page menu items
     const char* menu_items[PARAM_CH_LAST] = {
-        "Mod", "Probability", "Duty", "Offset", "CV Source", "CV Dest"};
+        "MOD", "PROBABILITY", "DUTY", "OFFSET", "CV SOURCE", "CV DEST"};
     drawMenuItems(menu_items, PARAM_CH_LAST);
 }
 
@@ -440,7 +484,7 @@ void DisplaySelectedChannel() {
             gravity.display.drawXBM(2, boxY, play_icon_width, play_icon_height, icon);
         } else {
             gravity.display.setFont(TEXT_FONT);
-            gravity.display.setCursor((i * boxWidth) + textOffset, SCREEN_HEIGHT - 1);
+            gravity.display.setCursor((i * boxWidth) + textOffset, SCREEN_HEIGHT - 3);
             gravity.display.print(i);
         }
     }
@@ -477,7 +521,7 @@ void drawMenuItems(const char* menu_items[], int menu_size) {
 
     for (int i = 0; i < min(menu_size, VISIBLE_MENU_ITEMS); ++i) {
         int idx = start_index + i;
-        drawRightAlignedText(menu_items[idx], MENU_ITEM_HEIGHT * (i + 1));
+        drawRightAlignedText(menu_items[idx], MENU_ITEM_HEIGHT * (i + 1) - 1);
     }
 }
 
